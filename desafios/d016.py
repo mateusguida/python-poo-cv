6c06c00d9a5d896bc463fe5c17f5be3f383ea161
mateusguida <guidamaster@gmail.com>
2026-02-09 19:13:05 -0300

    Correção e adendos - Desafio 016

--- desafios/d016.py
+++ desafios/d016.py
@@ -1,16 +1,24 @@
 from rich import print
 
 class Funcionario:
+    # Atributos de Classe
+    empresa = "Curso em Vídeo"
+
     def __init__(self, nome, setor, cargo):
+        # Atributos de instância
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
 
-    def apresentacao(self):
-        return f":right-facing_fist: Olá, sou [blue]{self.nome}[/blue] e sou {self.cargo} do setor de {self.setor} da empresa Curso em Vídeo."
+    def apresentacao(self) -> str:
+        return f":handshake: Olá, sou [blue]{self.nome}[/blue] e sou {self.cargo} do setor de {self.setor} da empresa {Funcionario.empresa}"
 
 c1 = Funcionario("Maria","Administração","Diretora")
+# inspect(c1, methods=True)
 print(c1.apresentacao())
 
 c2 = Funcionario("Pedro","TI","Programador")
+# inspect(c2, methods=True)
 print(c2.apresentacao())
+
+# inspect(Funcionario)
