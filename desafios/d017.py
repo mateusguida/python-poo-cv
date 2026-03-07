807d206cceab393dadfb5dc985dbc41fa15f0dac
mateusguida <guidamaster@gmail.com>
2026-03-07 15:13:41 -0300

    Pequenos ajustes dos desafios 17 e 18

--- desafios/d017.py
+++ desafios/d017.py
@@ -8,11 +8,12 @@ class Produto:
         self.preco = preco
 
     def etiqueta(self):
-        intervalo = "-" * 30
-        valor = f'R$ {self.preco:.2f}'
-        preco = f"{'.' * ((30 - len(valor))//2)}{valor}{'.' * ((30-len(valor))//2)}"
-        caixa = Panel(f"{self.nome}\n{intervalo}\n {preco}", title="Produto", width=35)
-        print(caixa)
+        conteudo = f"{self.nome.center(30, ' ')}"
+        conteudo += "-" * 30
+        valor = f'R$ {self.preco:,.2f}'
+        conteudo += f"\n{valor.center(30, ' ')}"
+        etiqueta = Panel(conteudo, title="Produto", width=34)
+        print(etiqueta)
 
 p1 = Produto("iPhone 17 Pro Max", 25_000.85)
 p2 = Produto("Notebook Gamer", 8_000)
--- desafios/d018.py
+++ desafios/d018.py
@@ -2,16 +2,33 @@ from rich import print
 from rich.panel import Panel
 
 class Churrasco:
+    # Atributos de classe
+    consumo_padrao =0.400
+    preco_kg = 82.40
     def __init__(self, nome_evento, num_pessoas):
         self.nome_evento = nome_evento
         self.num_pessoas = num_pessoas
 
-    def analisar(self):
-        total_carne = self.num_pessoas * 0.4  # kg por pessoa
-        custo_carne = total_carne * 82.40  # R$ 82,40 por kg
+    def __str__(self):
+        return f"Esse é o evento {self.nome_evento} com {self.num_pessoas} convidados"
 
-        caixa = Panel(f"Analisando [green]{self.nome_evento}[/green] com [blue]{self.num_pessoas} convidados[/blue]\nCada participante comerá 0.4 kg e cada kg custará R$ 82,40\nRecomendo [blue]comprar {total_carne:.3f} kg[/blue] de carne\nO custo total será de [green]R$ {custo_carne:.2f}[/green]\nCada pessoa pagará [yellow]R$ {custo_carne / self.num_pessoas:.2f}[/yellow] para participar", title = self.nome_evento)
+    def analisar(self):
+        total_carne = self.num_pessoas * self.consumo_padrao
+        custo_carne = total_carne * self.preco_kg
+
+        conteudo = f"Analisando [green]{self.nome_evento}[/] com [blue]{self.num_pessoas} convidados[/]"
+        conteudo += f"\nCada participante comerá 0.4 kg e cada kg custará R$ 82,40"
+        conteudo += f"\nRecomendo [blue]comprar {total_carne:.3f} kg[/] de carne"
+        conteudo += f"\nO custo total será de [green]R$ {custo_carne:,.2f}[/]"
+        conteudo += f"\nCada pessoa pagará [yellow]R$ {custo_carne / self.num_pessoas:,.2f}[/] para participar"
+
+        caixa = Panel(conteudo, title=self.nome_evento)
         print(caixa)
 
 c1 = Churrasco("Churras dos Amigos", 15)
+print(c1)
 c1.analisar()
+
+c2 = Churrasco("Festa do fim de ano", 80)
+print(c2)
+c2.analisar()
