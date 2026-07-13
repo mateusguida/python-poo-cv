bb4a12d769c9388e0c076d9c41711041501c2745
mateusguida <guidamaster@gmail.com>
2026-07-13 14:14:53 -0300

    Correção Desafio 31

--- desafios/d031/Retangulo.py
+++ desafios/d031/Retangulo.py
@@ -1,8 +1,12 @@
 class Retangulo:
     def __init__(self, base=1, altura=1):
-        self._base = base
-        self._altura = altura
-        self._area = base * altura
+        # parametros privados, com None, para evitar erros de atribuição direta
+        self._base = None
+        self._altura = None
+        self._area = None
+        # inicializa os atributos usando os setters para validação
+        self.base = base
+        self.altura = altura
 
     @property
     def base(self):
@@ -10,10 +14,12 @@ class Retangulo:
     
     @base.setter
     def base(self, valor):
-        if valor <= 0:
-            raise ValueError("A base deve ser maior que zero.")
-        self._base = valor
-        self._area = self._base * self._altura
+        if not isinstance(valor, (int, float)):
+            raise TypeError("O valor da altura deve ser um número.")
+        if valor < 0:
+            raise ValueError("Valor inválido para a base.")
+        else:
+            self._base = valor
 
     @property
     def altura(self):
@@ -21,27 +27,38 @@ class Retangulo:
     
     @altura.setter
     def altura(self, valor):
-        if valor <= 0:
-            raise ValueError("A altura deve ser maior que zero.")
-        self._altura = valor
-        self._area = self._base * self._altura
+        if not isinstance(valor, (int, float)):
+            raise TypeError("O valor da altura deve ser um número.")
+        if valor < 0:
+            raise ValueError("Valor inválido para a altura.")
+        else:
+            self._altura = valor
     
     @property
     def area(self):
+        self._area = self._base * self._altura
         return self._area
     
     @area.setter
     def area(self):
-        raise TypeError("A área não pode ser alterada diretamente.")
+        raise PermissionError("A área não pode ser alterada diretamente.")
     
     @property
     def medidas(self):
-        return (f"Base: {self._base}, Altura: {self._altura}, Área: {self._area}")
+        return (f"Base: {self.base}, Altura: {self.altura}, Área: {self.area}")
     
     @medidas.setter
-    def medidas(self, medidas):
-        if len(medidas) == 2:
-            if all(isinstance(m, (int, float)) for m in medidas) and all(m > 0 for m in medidas):
-              self._base = medidas[0]
-              self._altura = medidas[1]
-              self._area = self._base * self._altura
+    def medidas(self, valores:tuple):
+        if not isinstance(valores, tuple):
+            raise TypeError("As medidas devem ser passadas em uma tupla.")
+        if len(valores) != 2:
+            raise SyntaxError("A tupla deve conter exatamente dois valores: base e altura.")
+        if isinstance(valores[0], (int, float)):
+            self._base = valores[0]
+        else:
+            raise TypeError("O valor da base deve ser um número.")
+        if isinstance(valores[1], (int, float)):
+            self._altura = valores[1]
+        else:
+            raise TypeError("O valor da altura deve ser um número.")
+        
--- desafios/d031/__main__.py
+++ desafios/d031/__main__.py
@@ -2,17 +2,17 @@ from rich import inspect, print
 from Retangulo import Retangulo
 
 def main():
-  r = Retangulo(8,4)
-  r.altura = 12
-  r.base = 5
-  r.medidas = (-9, 3)
-  inspect(r, methods=True, private=True)
-  print(r.medidas)
+  r = Retangulo()
+  try:
+    r.altura = 7
+    r.base = 12
+    r.medidas = (3,9)
+  except Exception as e:
+    print(f"Ocorreu um erro do tipo {type(e).__name__}: {e}")
 
-  #try:
-    #r.area = 23
-  #except TypeError as e:
-    #print(f"[red]Erro: {e}[/red]")
+  #inspect(r, methods=True, private=True)
+  
+  print(r.medidas)
 
 if __name__ == "__main__":
   main()
